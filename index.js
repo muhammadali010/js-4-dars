6b1ccaa2d489e728aee1e13c411b821a067ac6fc
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-05-23 17:09:40 +0500

    boshladim

---
+++ index.js
@@ -0,0 +1,16 @@
+// 2chi masala
+// let a = + prompt("selsiy");
+// let kelvin =a+273.15;
+// let farangeyt= a * 1.80+32.00;
+// console.log(kelvin);
+// console.log(farangeyt);
+
+
+// 3chi masala
+let n =+ prompt("ali nech yosh kattaligini kiriting");
+let k =+ prompt("vali nech yosh kichikligini yozin");
+let val = vali
+let ali  = vali+n ;
+let vali  = ali -k;
+console.log(ali);
+console.log(vali);
