40d51b6b4d24932056b260818fe41047af2b1f34
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-05-24 04:20:33 +0500

    tugadi

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// 1chi masala
+// let t = + prompt ("vaqtni kiriting");
+// let T = + prompt ("vaqtni kirirting");
+// let vaqt = (t+T)%24;
+// console.log(vaqt);
+
+
 // 2chi masala
 // let a = + prompt("selsiy");
 // let kelvin =a+273.15;
@@ -6,11 +13,42 @@
 // console.log(farangeyt);
 
 
-// 3chi masala
-let n =+ prompt("ali nech yosh kattaligini kiriting");
-let k =+ prompt("vali nech yosh kichikligini yozin");
-let val = vali
-let ali  = vali+n ;
-let vali  = ali -k;
-console.log(ali);
-console.log(vali);
+// 4chi masala
+// let n = + prompt("kilogramni kiriting");
+// let a = + prompt("foizni kiriting");
+// let b = + prompt("foizni kiriting");
+// let c = (100*n-n*a)/(100-b);
+// console.log(c);
+
+
+// 5chi masala
+// let n = + prompt("tomonlar");
+// let = a=180*(n-2);
+// console.log(a);
+
+
+// 6chi masala
+// let hh = + prompt("soatni kiriting");
+// let mm = + prompt("minutni kiriting");
+// let t = + prompt("jami");
+// let m = t%60
+// let h = t/60;
+// h=parseInt(h);
+// if ((h+hh)>=0 && (h+hh)<=24){h=h+hh;
+// }else{
+// h=h+hh-24;
+// }
+// if ((m+mm)>=0 && (m+mm)<60){
+// m=m+mm;
+// }else{
+// m=m+mm-60;
+// h= h + 1
+// }
+// console.log(h,m);
+
+
+// 7chi masala
+// let buvi = + prompt("buvisini yoshini kiriting");
+// let sardor = + prompt("sardorni yoshini kiriting");
+// let natija =buvi-2*sardor;
+// console.log(natija);
